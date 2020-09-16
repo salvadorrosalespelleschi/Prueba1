2fb5851181be2e6f546e19c311fb969ca22702b9
Salvador Rosales Pelleschi <a2018114271@estudiantes.upsa.edu.bo>
2020-09-15 20:57:00 -0400

    Proyecto webApi con api web y JWT generado correctamente

---
+++ Pregunta1/Pregunta1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Pregunta1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Pregunta1/Pregunta1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pregunta1.WebApiApplication" Language="C#" %>
