d5da08ec09da33af065c9b547016419b2011895e
Salvador Rosales Pelleschi <a2018114271@estudiantes.upsa.edu.bo>
2020-09-15 21:38:17 -0400

    Modelos Creados

---
+++ Pregunta1/Pregunta1/Models/rosales.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace Pregunta1.Models
+{
+    public enum placeType { 
+        Casco_Viejo,
+        Burtown,
+        Plaza_24_de_septiembre,
+        Mangales,
+        Paradise
+    }
+
+    public class rosales
+    {
+        [Key]
+        public int rosalesID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        [StringLength(30, MinimumLength = 2)]
+        public string Friendofrosales { get; set; }
+        
+        [Required]
+        public placeType Place { get; set; }
+        
+
+        public string Email { get; set; }
+
+        [Display(Name = "Feliz Cumpleaños")]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
+        [DataType(DataType.Date]
+        public DateTime Birthdate { get; set; }
+        
+    }
+}
